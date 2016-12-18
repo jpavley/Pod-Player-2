9a4874e4fb222ddb2e9ba9831a60677224a08af4
John Pavley <johnpavley@johns-macbook-pro.local>
2016-12-18 00:17:24 -0500

    working on VC

--- Pod Player 2/PodcastViewController.swift
+++ Pod Player 2/PodcastViewController.swift
@@ -6,13 +6,29 @@
 //  Copyright © 2016 John Pavley. All rights reserved.
 //
 
+// NOTE: http://itunes.so-nik.com/
+// NOTE: http://codebeautify.org/xmlviewer
+
 import Cocoa
 
 class PodcastViewController: NSViewController {
+    
+    @IBOutlet weak var podcastTextField: NSTextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
     }
     
+    @IBAction func addPostcastClicked(_ sender: Any) {
+        if let url = URL(string: podcastTextField.stringValue) {
+            URLSession.shared.dataTask(with: url, completionHandler: { (data:Data?, response:URLResponse?, error:Error?) in
+                if error != nil {
+                    print(error)
+                } else {
+                    
+                }
+            })
+        }
+    }
 }
