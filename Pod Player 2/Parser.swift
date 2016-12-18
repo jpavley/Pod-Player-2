41e0b1741411e71bf41bb92e38a29338b8894e85
John Pavley <johnpavley@johns-macbook-pro.local>
2016-12-18 14:10:51 -0500

    added sw xml hash

---
+++ Pod Player 2/Parser.swift
@@ -0,0 +1,17 @@
+//
+//  Parser.swift
+//  Pod Player 2
+//
+//  Created by John Pavley on 12/18/16.
+//  Copyright © 2016 John Pavley. All rights reserved.
+//
+
+import Foundation
+
+class Parser {
+    
+    func getPodcastMetaData(data:Data) {
+        print(data)
+    }
+    
+}
--- Pod Player 2/PodcastViewController.swift
+++ Pod Player 2/PodcastViewController.swift
@@ -8,27 +8,35 @@
 
 // NOTE: http://itunes.so-nik.com/
 // NOTE: http://codebeautify.org/xmlviewer
+// NOTE: https://github.com/drmohundro/SWXMLHash
 
 import Cocoa
 
 class PodcastViewController: NSViewController {
     
     @IBOutlet weak var podcastTextField: NSTextField!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        
+        podcastTextField.stringValue = "http://www.espn.com/espnradio/podcast/feeds/itunes/podCast?id=2406595"
     }
     
     @IBAction func addPostcastClicked(_ sender: Any) {
         if let url = URL(string: podcastTextField.stringValue) {
             URLSession.shared.dataTask(with: url, completionHandler: { (data:Data?, response:URLResponse?, error:Error?) in
                 if error != nil {
-                    print(error)
+                    print(error!)
                 } else {
-                    
+                    if data != nil {
+                        let parser = Parser()
+                        parser.getPodcastMetaData(data: data!)
+                    }
                 }
-            })
+            }).resume()
+            
+            podcastTextField.stringValue = ""
         }
     }
 }
