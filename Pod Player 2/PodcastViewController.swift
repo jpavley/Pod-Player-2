ad954b9649f4bf65b061080dd3a1dc3feb952306
John Pavley <johnpavley@johns-macbook-pro.local>
2016-12-17 23:58:44 -0500

    podcast vc

---
+++ Pod Player 2/PodcastViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PodcastViewController.swift
+//  Pod Player 2
+//
+//  Created by John Pavley on 12/17/16.
+//  Copyright © 2016 John Pavley. All rights reserved.
+//
+
+import Cocoa
+
+class PodcastViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
